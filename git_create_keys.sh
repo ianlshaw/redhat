f669b6de74513b195c50b5559d64eed7b8eab23e
ianlshaw <ianshaw1991@gmail.com>
2015-05-20 13:59:44 +0100

    api import support for pubkey

--- git_create_keys.sh
+++ git_create_keys.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
-	#git_create_keys.sh
-	#Created by Ian Shaw
+!/bin/sh
+        #git_create_keys.sh
+        #Created by Ian Shaw
+echo "Input your OAuth token now:"
+read TOKEN
+PUBKEY=$(cat .ssh/id_rsa.pub)
+
+RESPONSE=`curl -s -H "Authorization: token ${TOKEN}" \
+  -X POST --data-binary "{\"title\":\"best@email\",\"key\":\"${PUBKEY}\"}" \
+  https://api.github.com/user/keys`
+
 echo -ne '\n' | ssh-keygen -t rsa -b 4096
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-cat .ssh/id_rsa.pub
-echo ""
-echo "Now copy the above public key into the Github web interface SSH keys section"
+echo "Done! - Verifying"
+ssh git@github.com
+
