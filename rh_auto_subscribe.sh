362478be351c0dd25dbc301703cc36525e9644dc
ianlshaw <ianshaw1991@gmail.com>
2015-05-21 10:26:02 +0100

    added firewall check

--- rh_auto_subscribe.sh
+++ rh_auto_subscribe.sh
@@ -2,6 +2,23 @@
 #Magical RH autosubscriber.
 #Writen by Ian Shaw
 
+#Functions
+function check_firewall() {
+        rport=$(timeout 1 bash -c '>/dev/tcp/193.133.96.34/80' &&   echo "Port open" ||   echo "Port closed")
+        echo $rport
+        if [ "$rport" == "Port closed" ] ; then
+                echo "This machine does not have firewall access to the Web Proxy."
+                exit 0
+        else
+                echo "Proxy port is open, web traffic should be allowed to pass."
+
+
+        fi
+}
+
+check_firewall
+
+
 #Purge existing repos
 rm -f /etc/yum.repos.d/*
 
